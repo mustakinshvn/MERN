14aad7d0b1afac4d5ffb0761c9f2b46f421c4b6c
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-10-11 02:35:52 +0600

    install reactJs and tailwindd css

---
+++ AuthApp/client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="p-10 bg-gray-200">
+      <h1 className="text-3xl font-bold text-center text-gray-800">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+  )
+}
---
+++ ToDoList/toDoList/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Create from './Create'
+
+function Home() {
+  return (
+    <div>
+      <h2>ToDO List</h2>
+      <Create/>
+    </div>
+  )
+}
+
+export default Home
