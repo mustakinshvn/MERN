d34a885e1e890b324b88e3991e3bd6b8aa170fac
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-10-11 03:32:59 +0600

    Create Header components

--- AuthApp/client/src/App.jsx
+++ AuthApp/client/src/App.jsx
@@ -4,9 +4,12 @@ import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+  {/* header */}
+  <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
