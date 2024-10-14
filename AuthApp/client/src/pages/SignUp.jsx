eec15991927edb2afe6b673e4237e51ab634e3c5
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-10-14 23:26:54 +0600

    complete SignUp page UI

--- AuthApp/client/src/pages/SignUp.jsx
+++ AuthApp/client/src/pages/SignUp.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
+    <div className='p-3 max-w-lg mx-auto'>
+    <h1 className = 'text-3xl text-center font-semibold my-7'>Sign Up</h1>
+    
+    <form className= 'flex flex-col gap-4 '>
+      <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg '  />
+      <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg  '  />
+      <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg '/>
+      <button className='bg-slate-700  text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>Sign Up</button>
+    </form>
+
     <div>
-      SignUp
+      <p className='text-center mt-5'>Already have an account? <a href='/signIn' className='text-blue-700'>Login here</a></p>
+    </div>
+
     </div>
   )
 }
