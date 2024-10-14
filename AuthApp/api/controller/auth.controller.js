2b359dc2eb5f1a93c114841b62b1c711c699c6b2
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-10-14 18:21:56 +0600

    create sign up api and hashPassword

---
+++ AuthApp/api/controller/auth.controller.js
@@ -0,0 +1,19 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+export  const signup = async (req, res) => {  
+    const { userName,  password, email } = req.body;
+    const hashedPassword = await bcryptjs.hash(password, 12);
+    const newUser = new User({
+        userName,
+        password : hashedPassword,
+        email
+        }); 
+
+        try{     
+       await newUser.save();
+       res.status(201).json({message: 'User created successfully'});
+        }catch(error){
+            res.status(409).json({message: error.message});
+        }
+
+}
--- AuthApp/api/index.js
+++ AuthApp/api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -9,6 +10,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => { console.log(err); });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
@@ -16,3 +18,4 @@ app.listen(3000, () => {
 
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
